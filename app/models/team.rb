516518d921775fc850829d9f3317557d9dcc44e2
Miroslav Rachev <miro@mirosr.net>
2014-09-19 22:35:49 +0300

    Change Team.all to return real data from a file

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,13 @@
 Team = Struct.new(:id, :name) do
-  def self.all
-    []
+  def self.all(filename)
+    filename = Rails.root.join('spec', 'fixtures', filename)
+
+    File.open(filename).each_line.select {|item| item =~ /^\s+\d/}.map do |line|
+      cols = line.chomp.split(/\s{2,}/)
+
+      id, name = cols[1].split('. ')
+
+      new(id, name)
+    end
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,8 +3,18 @@ RSpec.describe Team do
   it { is_expected.to respond_to(:name) }
 
   describe '.all' do
-    it 'acts as an array' do
-      expect(Team.all).to respond_to(:map)
+    it 'returns an array with team info from a file' do
+      expect(Team.all('football.dat')).to eq(fixture_array)
     end
+  end
+
+  private
+
+  def fixture_array
+    [
+      Team.new('1', 'Arsenal'),
+      Team.new('2', 'Liverpool'),
+      Team.new('3', 'Manchester_U')
+    ]
   end
 end
