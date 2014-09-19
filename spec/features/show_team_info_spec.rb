108f1e1d3bc626abfd3f4a911d610c2f441882ec
Miroslav Rachev <miro@mirosr.net>
2014-09-19 20:54:29 +0300

    Add initial 'Select team' form

--- spec/features/show_team_info_spec.rb
+++ spec/features/show_team_info_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature 'Team info' do
 
     expect(current_path).to eq root_path
 
+    expect(page).to have_text 'Team Info'
     expect(page).to have_field 'Select team'
     expect(page).to have_button 'Show'
     expect(page).to have_text 'No team is selected'
